e9e8017c85f084169ecba98f39ab18f9e65ae683
ericcolgan86 <35762341+ericcolgan86@users.noreply.github.com>
2018-12-23 11:59:56 +0000

    Initial commit

---
+++ src/main/java/controllers/PacemakerRestService.java
@@ -0,0 +1,101 @@
+package controllers;
+
+import io.javalin.Context;
+import models.Activity;
+import models.Location;
+import models.User;
+
+import static models.Fixtures.users;
+
+public class PacemakerRestService {
+
+  PacemakerAPI pacemaker = new PacemakerAPI();
+
+  PacemakerRestService() {
+    users.forEach(
+        user -> pacemaker.createUser(user.firstname, user.lastname, user.email, user.password));
+  }
+
+  public void listUsers(Context ctx) {
+    ctx.json(pacemaker.getUsers());
+    System.out.println("list users requested");
+  }
+  
+
+
+	public void createUser(Context ctx) {
+		User user = ctx.bodyAsClass(User.class);
+	    User newUser = pacemaker
+	        .createUser(user.firstname, user.lastname, user.email, user.password);
+	    ctx.json(newUser);	
+	    System.out.println("createUser requested");
+	}
+	
+	 public void listUser(Context ctx) {
+		    String id = ctx.pathParam("id");
+		    ctx.json(pacemaker.getUser(id));
+		    System.out.println("listUser requested");
+	 }
+	 
+	 
+	 public void getActivities(Context ctx) {
+		    String id = ctx.pathParam("id");
+		    User user = pacemaker.getUser(id);
+		    if (user != null) {
+		      ctx.json(user.activities.values());
+		      System.out.println("getActivities requested");
+		    } else {
+		      ctx.status(404);
+		    }
+		  }
+
+	 public void createActivity(Context ctx) {
+		    String id = ctx.pathParam("id");
+		    User user = pacemaker.getUser(id);
+		    if (user != null) {
+		      Activity activity = ctx.bodyAsClass(Activity.class);
+		      Activity newActivity = pacemaker
+		          .createActivity(id, activity.type, activity.location, activity.distance);
+		      ctx.json(newActivity);
+		      System.out.println("createActivity requested");
+		    } else {
+		      ctx.status(404);
+		    }
+		  }
+	 
+	  public void getActivity(Context ctx) {
+		    String id = ctx.pathParam("activityId");
+		    Activity activity = pacemaker.getActivity(id);
+		    if (activity != null) {
+		      ctx.json(activity);
+		      System.out.println("getActivity requested");
+		    } else {
+		      ctx.status(404);
+		    }
+		  }
+	  
+	  
+	  public void getActivityLocations(Context ctx) {
+		    String id = ctx.pathParam("activityid");
+		    Activity activity = pacemaker.getActivity(id);
+		    if (activity != null) {
+		      ctx.json(activity.route);
+		      System.out.println("getActivityLocations requested");
+		    } else {
+		      ctx.status(404);
+		    }
+		  }
+
+		  public void addLocation(Context ctx) {
+		    String id = ctx.pathParam("activityid");
+		    Activity activity = pacemaker.getActivity(id);
+		    if (activity != null) {
+		      Location location = ctx.bodyAsClass(Location.class);
+		      activity.route.add(location);
+		      ctx.json(location);
+		      System.out.println("addLocation requested");
+		    } else {
+		      ctx.status(404);
+		    }
+		  }
+}
---
+++ src/main/java/controllers/RestMain.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import io.javalin.Javalin;
+
+public class RestMain {
+
+  public static void main(String[] args) throws Exception {
+    Javalin app = Javalin.create().start(7000);
+    PacemakerRestService service = new PacemakerRestService();
+    configRoutes(app, service);
+  }
+
+  static void configRoutes(Javalin app, PacemakerRestService service) {
+
+    app.get("/users", ctx -> {
+      service.listUsers(ctx);
+    });
+    
+    app.post("/users", ctx -> {
+        service.createUser(ctx);
+      });
+    
+    app.get("/users/:id", ctx -> {
+        service.listUser(ctx);
+      });
+    
+    app.get("/users/:id/activities", ctx -> {
+        service.getActivities(ctx);
+      });
+
+    app.post("/users/:id/activities", ctx -> {
+        service.createActivity(ctx);
+      });
+    
+    app.get("/users/:id/activities/:activityid", ctx -> {
+        service.getActivity(ctx);
+      });
+    
+    app.get("/users/:id/activities/:activityid/locations", ctx -> {
+        service.getActivityLocations(ctx);
+      });
+
+      app.post("/users/:id/activities/:activityid/locations", ctx -> {
+        service.addLocation(ctx);
+      });
+  }
+}
